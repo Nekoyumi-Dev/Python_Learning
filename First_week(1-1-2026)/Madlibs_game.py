46a92ba161b201b2e2e5541f407a56276a1fb8fa
Nekoyumi <nghphuc404@gmail.com>
2026-01-01 19:00:51 +0700

    madlibs game

---
+++ First_week(1-1-2026)/Madlibs_game.py
@@ -0,0 +1,21 @@
+# Adj 1, 2, 3
+# N 1, 2, 3
+# V 1, 2, 3
+adj1 = input("Enter an adjective: ")
+adj2 = input("Enter an adjective: ")
+adj3 = input("Enter an adjective: ")
+n1 = input("Enter a noun: ")
+n2 = input("Enter a noun: ")
+n3 = input("Enter a noun: ")
+v1 = input("Enter a verb: ")
+v2 = input("Enter a verb: ")
+v3 = input("Enter a verb: ")
+
+
+print(f"Hi, I'm a {n1}, i like to {v1} with you, darling.")
+print(f"I'm very {adj1} and I love my {n2}.")
+print(f"Sometimes I {v2} when I'm feeling {adj2}.")
+print(f"But don't worry, my {n3} is always there to help me.")
+print(f"In the end, we all just want to {v3} and be {adj3}.")
+
+#fun 
